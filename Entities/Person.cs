1c4f523ba100c2043d8f96fc9d37507076de03fc
Igor Gabriel <souzaamorimdasilvai@gmail.com>
2024-08-02 18:21:52 -0300

    Created abstract class of person

---
+++ Entities/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace BankAccount.Entities
+{
+    abstract class Person
+    {
+        public string Name { get; private set; }
+        public double AnnualIncome { get; private set; }
+
+        public Person(string name, double annualIncome)
+        {
+            Name = name;
+            AnnualIncome = annualIncome;
+        }
+
+        public abstract double Tax() ;
+    }
+}
---
+++ Entities/PersonPhysical.cs
@@ -0,0 +1,25 @@
+﻿namespace BankAccount.Entities
+{
+    class PersonPhysical : Person
+    {
+        public double HealthSpending { get; private set; }
+
+        public PersonPhysical(double healthSpending, string name, double anualIncome) 
+            : base(name, anualIncome) 
+        {
+            HealthSpending = healthSpending;
+        }
+
+        public override double Tax()
+        {
+            double tax = 0;
+
+            if(AnnualIncome < 20000.00)
+            {
+                tax =  
+            } 
+
+            return tax;
+        }
+    }
+}
